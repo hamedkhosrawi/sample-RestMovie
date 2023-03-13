4bcc0f71e5a4586cc415c3fe754100136f6012bd
hamedkhosrawi <khosrawi.hame2@gmail.com>
2023-03-13 12:03:41 +0330

    first commit

---
+++ src/main/java/org/home/tutorial/quarkusmaven/MovieResource.java
@@ -0,0 +1,62 @@
+package org.home.tutorial.quarkusmaven;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/movies")
+public class MovieResource {
+
+    public static List<String> movies = new ArrayList<>();
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMovies(){
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies(){
+        return movies.size();
+    }
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response createMovie(String newMovie){
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+    }
+
+    @PUT
+    @Path("{movieToUpdate}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateMovie(
+            @PathParam("movieToUpdate") String movieToUpdate,
+            @QueryParam("movie") String updateMovie ){
+            movies = movies.stream().map(movie -> {
+                if (movie.equals(movieToUpdate)){
+                    return updateMovie;
+                }
+                return movie;
+            }).collect(Collectors.toList());
+            return Response.ok(movies).build();
+
+    }
+
+
+    @DELETE
+    @Path("{movieToDelete}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response deleteMovie(
+            @PathParam("movieToDelete") String movieToDelete
+    ){
+        boolean removed = movies.remove(movieToDelete);
+        return removed ? Response.noContent().build() :
+                Response.status(Response.Status.BAD_REQUEST).build();
+    }
+}
---
+++ src/test/java/org/home/tutorial/quarkusmaven/HelloQuarkusIT.java
@@ -0,0 +1,8 @@
+package org.home.tutorial.quarkusmaven;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloQuarkusIT extends HelloQuarkusTest {
+    // Execute the same tests but in packaged mode.
+}
